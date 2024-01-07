50b9babb7009d00a3c538515a6d1ecffa112ef63
emmamsiska01 <msiskaemma01@gmail.com>
2024-01-07 22:47:32 +0200

    Added javascript

---
+++ src/index.js
@@ -0,0 +1,45 @@
+function displayCity(response) {
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature");
+  let temperatureValue = response.data.temperature.current;
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let timeElement = document.querySelector("#current_time");
+  let date = new Date(response.data.time * 1000);
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperatureValue);
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  timeElement.innerHTML = formatDate(date);
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}: ${minutes}`;
+}
+function findCityTemperature(city) {
+  let apiKey = "72a0433c33e7oa3t6184fd0dd47b8e9b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayCity);
+}
+function searchSubmit(event) {
+  event.preventDefault();
+  let SearchInput = document.querySelector("#city_name");
+  findCityTemperature(SearchInput.value);
+}
+let searchCity = document.querySelector("#search_form");
+searchCity.addEventListener("submit", searchSubmit);
